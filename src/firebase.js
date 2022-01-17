c69448a4bab02cbf8794901a19c1d5d5617cb14b
Abdullah Moursi <moursiabdullah@gmail.com>
2022-01-17 18:01:15 +0200

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDJG2-OTUvZYe-e0OoqH2Wm8ugjP-5WKek",
+    authDomain: "challenge-fe330.firebaseapp.com",
+    projectId: "challenge-fe330",
+    storageBucket: "challenge-fe330.appspot.com",
+    messagingSenderId: "160279317809",
+    appId: "1:160279317809:web:97d01ffe19e717f62e7410",
+    measurementId: "G-DY7BE91S3T"
+  };
