d5f296d2b7fde0e8875b00f7d6c96b6e7eda32bc
Abdullah Moursi <moursiabdullah@gmail.com>
2022-01-18 21:35:16 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import react from 'react';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    <Header />
-    <Home />
-    </div>
+    <Router>
+      <div className="App">
+        {" "}
+        <Header />
+        <Switch>
+          <Route exact path="/checkout">
+            <h1>checkout</h1>
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>{" "}
+    </Router>
   );
 }
 
